15f843c97534e255c9b56fbc0abf50f2dd2618d4
RickyMatthews <matthewsrickypro@gmail.com>
2023-03-15 10:55:52 +0300

    Full Document set up. Components, Pages and routes

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -3,10 +3,27 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { FiSettings } from "react-icons/fi";
 import { TooltipComponent } from "@syncfusion/ej2-react-popups";
 
+import { Navbar, Footer, Sidebar, ThemeSettings } from "./components";
+import {
+	ECommerce,
+	Orders,
+	Calendar,
+	Employees,
+	Stacked,
+	Pyramid,
+	Customers,
+	Kanban,
+	Area,
+	Bar,
+	Pie,
+	Financial,
+	ColorPicker,
+	ColorMapping,
+	Editor,
+} from "./pages";
 import "./App.css";
 
 const App = () => {
-
 	const activeMenu = true;
 	return (
 		<div>
@@ -27,18 +44,17 @@ const App = () => {
 						</TooltipComponent>
 					</div>
 					{activeMenu ? (
-						<div className="w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white">
+						<div className='w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white'>
 							Sidebar
 						</div>
 					) : (
-						<div className="w-0 dark:bg-secondary-dark-bg">
-							Sidebar
-						</div>
+						<div className='w-0 dark:bg-secondary-dark-bg'>Sidebar</div>
 					)}
-					<div className={
-						`dark:bg-main-bg bg-main-bg min-h-screen w-full ${activeMenu ? 'md:ml-72' : 'flex'}`
-					}>
-						<div className="fixed md:stati bg-main-bg dark:bg-main-dark-bg navbar w-full">
+					<div
+						className={`dark:bg-main-bg bg-main-bg min-h-screen w-full ${
+							activeMenu ? "md:ml-72" : "flex"
+						}`}>
+						<div className='fixed md:stati bg-main-bg dark:bg-main-dark-bg navbar w-full'>
 							Navbar
 						</div>
 					</div>
@@ -46,34 +62,85 @@ const App = () => {
 					<div>
 						<Routes>
 							{/* Dashboard */}
-							<Route path="/" element="ECommerce" />
-							<Route path="/ecommerce" element="ECommerce" />
+							<Route
+								path='/'
+								element='ECommerce'
+							/>
+							<Route
+								path='/ecommerce'
+								element='ECommerce'
+							/>
 
 							{/* pages */}
-							<Route path="/orders" element="Orders" />
-							<Route path="/employees" element="Employees" />
-							<Route path="/customers" element="Customers" />
+							<Route
+								path='/orders'
+								element='Orders'
+							/>
+							<Route
+								path='/employees'
+								element='Employees'
+							/>
+							<Route
+								path='/customers'
+								element='Customers'
+							/>
 
 							{/* Apps */}
-							<Route path="/kanban" element="Kanban" />
-							<Route path="/editor" element="Editor" />
-							<Route path="/calender" element="Calendar" />
-							<Route path="/color-picker" element="ColorPicker" />
+							<Route
+								path='/kanban'
+								element='Kanban'
+							/>
+							<Route
+								path='/editor'
+								element='Editor'
+							/>
+							<Route
+								path='/calender'
+								element='Calendar'
+							/>
+							<Route
+								path='/color-picker'
+								element='ColorPicker'
+							/>
 
 							{/* Charts */}
-							<Route path="/line" element="Line" />
-							<Route path="/area" element="Area" />
-							<Route path="/bar" element="Bar" />
-							<Route path="/pie" element="Pie" />
-							<Route path="/financial" element="Financial" />
-							<Route path="/color-mapping" element="ColorMapping" />
-							<Route path="/pyramid" element="Pyramid" />
-							<Route path="/stacked" element="Stacked" />
+							<Route
+								path='/line'
+								element='Line'
+							/>
+							<Route
+								path='/area'
+								element='Area'
+							/>
+							<Route
+								path='/bar'
+								element='Bar'
+							/>
+							<Route
+								path='/pie'
+								element='Pie'
+							/>
+							<Route
+								path='/financial'
+								element='Financial'
+							/>
+							<Route
+								path='/color-mapping'
+								element='ColorMapping'
+							/>
+							<Route
+								path='/pyramid'
+								element='Pyramid'
+							/>
+							<Route
+								path='/stacked'
+								element='Stacked'
+							/>
 						</Routes>
 					</div>
 				</div>
-			</BrowserRouter >
-		</div >
+			</BrowserRouter>
+		</div>
 	);
 };
 
--- dashboard/src/components/SIdebar.jsx
+++ dashboard/src/components/SIdebar.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-const SIdebar = () => {
+const Sidebar = () => {
   return (
-    <div>SIdebar</div>
+    <div>Sidebar</div>
   )
 }
 
-export default SIdebar
+export default Sidebar
