bc25b318e07a8873dc455389e1b4aed54e1d49ea
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2023-03-27 21:33:33 +0300

    Update Navbar.jsx

--- dashboard/src/components/Navbar.jsx
+++ dashboard/src/components/Navbar.jsx
@@ -65,6 +65,20 @@ const Navbar = () => {
           
           />
 
+          <TooltipComponent
+            content="Profile"
+            position='BottomCenter'
+           >
+            <div className="flex items-center gap-2 cursor-pointer p-1 hover:bg-light-gray rounded-lg"
+            onClick={() => handleClick('userProfile')
+            }>
+              <img 
+              className="rounded-full w-8 h-8"
+              src={avatar} alt = 'propic'
+              />
+              <p></p>
+            </div>
+           </TooltipComponent>
         
        
 
