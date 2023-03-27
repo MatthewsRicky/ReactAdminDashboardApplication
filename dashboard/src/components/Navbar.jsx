228dc7b21a8df721e56e06534f1789f3c287af97
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2023-03-27 21:00:01 +0300

    Update Navbar.jsx

--- dashboard/src/components/Navbar.jsx
+++ dashboard/src/components/Navbar.jsx
@@ -11,7 +11,8 @@ import { Chat, Cart, Notifications, userProfile } from '.'
 import { useStateContext } from '../contexts/ContextProvider'
 
 const NavButton = ({ title, customFunc, icon, color, dotColor}) => (
-  <TooltipComponent content={title}
+  <TooltipComponent 
+  content={title}
   position="BottomCenter">
     <button
     type='button' 
@@ -19,9 +20,9 @@ const NavButton = ({ title, customFunc, icon, color, dotColor}) => (
     style={{ color }}
     className="relative text-xl rounded-full p-3 hover:bg-light-gray">
     <span 
-    style={{Background: dotColor}}
+    style={{ background: dotColor }}
     className="absolute inline-flex rounded-full h-2 w-2 right-2 top-2">
-      {icon}
+      { icon }
     </span>
     </button>
   </TooltipComponent>
@@ -31,7 +32,11 @@ const Navbar = () => {
   const { activeMenu, setActiveMenu } = useStateContext()
   return (
     <div className='flex justify-between p-2 md:mx-6 relative'>
-      <NavButton title="Menu" customFunc ={() => setActiveMenu((prevActiveMenu)=>!prevActiveMenu)} color="blue" icon={ AiOutlineMenu}/>
+      <NavButton 
+        title="Menu" 
+        customFunc ={() => setActiveMenu((prevActiveMenu)=>!prevActiveMenu)} 
+        color="blue" 
+        icon={ AiOutlineMenu}/>
     </div>
   )
 }
