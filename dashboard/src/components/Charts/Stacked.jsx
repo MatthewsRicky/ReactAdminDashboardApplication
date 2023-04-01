a80b89b32480be2fa3afcf2e9de684f25c2006f9
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2023-04-01 22:54:36 +0300

    Update Stacked.jsx

--- dashboard/src/components/Charts/Stacked.jsx
+++ dashboard/src/components/Charts/Stacked.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import { ChartComponent, SeriesDirective, Inject, Legend, Category, StackingColumnSeries, Tooltip } from '@syncfusion/ej2-react-charts'
 const Stacked = () => {
   return (
     <div>Stacked</div>
