27abddfaf600131e3b9f33ac8e77953e25a09c9b
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2023-04-01 22:50:17 +0300

    Update ECommerce.jsx

--- dashboard/src/pages/ECommerce.jsx
+++ dashboard/src/pages/ECommerce.jsx
@@ -108,8 +108,20 @@ const Ecommerce = () => {
                       width="250px"
                       data={SparklineAreaData}
                       color="blue"
+                     />
+                  </div>
+                  <div className="mt-10">
+                    <Button 
+                      color='white'
+                      bgColor='blue'
+                      text='Download Report'
+                      borderRadius='10px'
                     />
                   </div>
+              </div>
+              <div>
+                <Stacked width="320px"
+                 height="360px" />
               </div>
             </div>  
           </div>
