96283eeb312724d32e4d63a622ea66064bfe5148
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2023-04-01 13:24:51 +0300

    Update ECommerce.jsx

--- dashboard/src/pages/ECommerce.jsx
+++ dashboard/src/pages/ECommerce.jsx
@@ -84,8 +84,27 @@ const Ecommerce = () => {
             <div className="border-1-1 border-color m-4 pr-10">
               <p>
                 <span className='text-3xl font-semibold'>$93,438</span>
-                <span className='p-1.5 hover:drop-shadow-xl cursor-pointer rounded-full text-white bg-green-400'>23%</span>
+                <span className='p-1.5 hover:drop-shadow-xl cursor-pointer rounded-full text-white bg-green-400 ml-3 text-xs'>23%</span>
               </p>
+              <p className="text-gray-500 mt-1">Budget</p>
+            </div>
+            <div className="border-1-1 border-color m-4 pr-10">
+              <p>
+                <span className='text-3xl font-semibold'>$48,438</span>
+              </p>
+              <p className="text-gray-500 mt-1">Expense</p>
+            </div>
+
+            <div className="mt-5">
+              <SparkLine 
+                currentColor="blue"
+                id="line-sparkline"
+                type="Line"
+                height="80px"
+                width="250px"
+                data={SparklineAreaData}
+                color="blue"
+              />
             </div>
           </div>
         </div>
