7a4de5ee7c9ebf315d4c08f0fba1ba159470a521
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2023-04-02 22:57:35 +0300

    Update Calendar.jsx

--- dashboard/src/pages/Calendar.jsx
+++ dashboard/src/pages/Calendar.jsx
@@ -8,7 +8,12 @@ import { Header } from '../components'
 
 const Calendar = () => {
   return (
-    <div className='m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl'></div>
+    <div className='m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl'>
+      <Header category="App" title="Calender" />
+      <ScheduleComponent>
+        <Inject />
+      </ScheduleComponent>
+    </div>
   )
 }
 
