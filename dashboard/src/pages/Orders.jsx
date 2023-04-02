fd2647e926770c59adfacba705b2eae047cb56b5
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2023-04-02 14:46:57 +0300

    Update Orders.jsx

--- dashboard/src/pages/Orders.jsx
+++ dashboard/src/pages/Orders.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { GridComponent, ColumnsDirective, ColumnDirective, Resize,  Sort, ContextMenu, Filter, Page, ExcelExport, PdfExport, Edit, Inject } from '@syncfusion/ej2-react-grids'
 
 const Orders = () => {
   return (
