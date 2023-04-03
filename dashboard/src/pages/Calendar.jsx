1044d389a989ea42dbd3bab4f863a0782633c101
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2023-04-03 17:07:15 +0300

    Update Calendar.jsx

--- dashboard/src/pages/Calendar.jsx
+++ dashboard/src/pages/Calendar.jsx
@@ -11,7 +11,7 @@ const Calendar = () => {
     <div className='m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl'>
       <Header category="App" title="Calender" />
       <ScheduleComponent>
-        <Inject />
+        <Inject services={[Day, Week, WorkWeek, Month, Agenda, Resize, DragAndDrop]}/>
       </ScheduleComponent>
     </div>
   )
