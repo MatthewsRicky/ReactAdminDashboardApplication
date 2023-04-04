e41eb86a35c9c69618bc7d47f761cd9377d510ce
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2023-04-04 14:03:25 +0300

    Update LineChart.jsx

--- dashboard/src/components/Charts/LineChart.jsx
+++ dashboard/src/components/Charts/LineChart.jsx
@@ -3,13 +3,20 @@ import { ChartComponent, SeriesCollectionDirective, SeriesDirective, Inject, Dat
 
 import { lineCustomSeries, LinePrimaryXAxis, LinePrimaryYAxis } from '../../data/dummy'
 
+import { useStateContext } from '../../contexts/ContextProvider'
 
 const LineChart = () => {
+  const { currentMode  } = useStateContext()
+
   return (
     <ChartComponent
       id='line-chart'
       height='420px'
       primaryXAxis={LinePrimaryXAxis}
+      primaryYAxis={LinePrimaryYAxis}
+      chartArea={{ border: { width: 0 }}}
+      tooltip={{ enable : true }}
+      background={currentMode === 'Dark' ? '#33373E' : '#fff'}
     >
       <Inject services={[LineSeries, DateTime, Legend, Tooltip]} />
       <SeriesCollectionDirective>
