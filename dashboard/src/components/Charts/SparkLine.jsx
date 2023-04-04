b7cda23448eed2b7203128d16719cb040b7c629f
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2023-04-04 17:14:04 +0300

    Update SparkLine.jsx

--- dashboard/src/components/Charts/SparkLine.jsx
+++ dashboard/src/components/Charts/SparkLine.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
 import { SparklineComponent, Inject, SparklineTooltip } from '@syncfusion/ej2-react-charts'
 
-const SparkLine = ({ id, height, width, color, data, type, currentColor}) => {
+class SparkLine extends  React.PureComponent {
+  render() {
+   const { id, height, width, color, data, type, currentColor } = this.props
+
   return (
     <SparklineComponent
       id={id}
@@ -17,7 +20,7 @@ const SparkLine = ({ id, height, width, color, data, type, currentColor}) => {
       type={type}
       tooltipSettings={{
         visible: true,
-        format: "${xval} : data ${yval}",
+        format: '${xval} : data ${y}',
         trackLineSettings : {
           visible: true
         }
@@ -26,5 +29,7 @@ const SparkLine = ({ id, height, width, color, data, type, currentColor}) => {
     </SparklineComponent>
   )
 }
+}
+ 
 
 export default SparkLine
