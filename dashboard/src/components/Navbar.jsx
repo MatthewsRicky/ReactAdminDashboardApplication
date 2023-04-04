2f0b77fdefe7a2630bac479ba696acf1799b515e
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2023-04-04 11:15:08 +0300

    Update Navbar.jsx

--- dashboard/src/components/Navbar.jsx
+++ dashboard/src/components/Navbar.jsx
@@ -28,7 +28,7 @@ const NavButton = ({ title, customFunc, icon, color, dotColor}) => (
 )
 
 const Navbar = () => {
-  const { activeMenu, setActiveMenu, isClicked, setIsClicked, handleClick, screenSize, setScreenSize} = useStateContext()
+  const { activeMenu, setActiveMenu, isClicked, setIsClicked, handleClick, screenSize, setScreenSize, currentColor} = useStateContext()
 
   useEffect(() => {
     const handleResize = () => setScreenSize(window.innerWidth)
@@ -56,7 +56,7 @@ const Navbar = () => {
           <NavButton 
             title="Cart" 
             customFunc ={() => handleClick('cart')} 
-            color="blue" 
+            color={currentColor} 
             icon={ <FiShoppingCart />}
           
           />
@@ -66,7 +66,7 @@ const Navbar = () => {
             title="Chat"
             dotColor="#03c9d7" 
             customFunc ={() => handleClick('chat')} 
-            color="blue" 
+            color={currentColor} 
             icon={ <BsChatLeft />}
           
           />
@@ -74,7 +74,7 @@ const Navbar = () => {
             title="Notification"
             dotColor="#03c9d7" 
             customFunc ={() => handleClick('notification')} 
-            color="blue" 
+            color={currentColor}
             icon={ <RiNotification3Line />}
           
           />
