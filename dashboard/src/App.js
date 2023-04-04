270aeb79da40d3f2be9d1fe1b7e3ee294effdb27
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2023-04-04 12:03:43 +0300

    Update App.js

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -28,11 +28,11 @@ import { useStateContext } from "./contexts/ContextProvider";
 import "./App.css";
 
 const App = () => {
-	const {activeMenu, themeSettings, setThemeSettings, currentColor} = useStateContext();
+	const {activeMenu, themeSettings, setThemeSettings, currentColor, currentMode} = useStateContext();
 	return (
-		<div>
+		<div className={currentMode === 'Dark' ? 'dark' : ''}>
 			<BrowserRouter>
-				<div className='flex relative dark:bg-main-dar-bg'>
+				<div className='flex relative dark:bg-main-dark-bg'>
 					<div
 						className='fixed right-4 bottom-4'
 						style={{ zIndex: "1000" }}>
@@ -59,8 +59,10 @@ const App = () => {
 						</div>
 					)}
 					<div
-						className={`dark:bg-main-bg bg-main-bg min-h-screen w-full ${
-							activeMenu ? "md:ml-72" : "flex"
+						className={`dark:bg-main-dark-bg bg-main-bg min-h-screen w-full ${
+							activeMenu 
+							? "md:ml-72" 
+							: "flex"
 						}`}>
 						<div className='fixed md:stati bg-main-bg dark:bg-main-dark-bg navbar w-full'>
 							<Navbar />
