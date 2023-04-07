8f76cecc42cfc2e8cf2354f2ed9407ce314d05fa
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2023-04-08 00:41:28 +0300

    Update Pie.jsx

--- dashboard/src/pages/Charts/Pie.jsx
+++ dashboard/src/pages/Charts/Pie.jsx
@@ -1,25 +1,30 @@
 import React from 'react'
-import { ChartComponent, SeriesCollectionDirective, SeriesDirective, Inject, DataLabel, Legend, Category, Tooltip, ColumnSeries } from '@syncfusion/ej2-react-charts'
+import { SeriesCollectionDirective, SeriesDirective, Inject, DataLabel, Legend, Category, Tooltip } from '@syncfusion/ej2-react-charts'
 import { FiPieChart } from 'react-icons/fi'
 
-import { pieCustomSeries, piePrimaryXAxis, barPrimaryYAxis } from '../../data/dummy'
 
-import { pieChartData } from '../../data/dummy'
+
+import {  ecomPieChartData } from '../../data/dummy'
 import { Header,  Pie as PieChart } from '../../components'
 
 const Pie = () =>  (
   <div className="m-4 md:m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl">
-    <Header category='Pie' title='project Cost breakdown' />
+    <Header category='Pie' title='Project Cost Breakdown' />
     <div className="w-full">
-      <PieChart id='chart-pie' data={pieChartData} legend visible height='full' />
-      <pie
-      id='pie'
-      height='420'
 
-      >
-    
-
-      </pie>
+      <PieChart 
+        id='chart-pie' 
+        data={ecomPieChartData} 
+        legend   
+        visible height='full'
+        chartArea={{ border: { width: 0 }}}
+        tooltip={{ enable : true }} />
+      <Inject services={[DataLabel, Legend, Category,  Tooltip]} />
+      <SeriesCollectionDirective>
+          {ecomPieChartData.map((item, index) => 
+          <SeriesDirective key={index} {...item} />
+          )}
+        </SeriesCollectionDirective>
     </div>
   </div>
 )
