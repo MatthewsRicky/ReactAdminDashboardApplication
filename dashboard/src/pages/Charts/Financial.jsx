e3fda5013477b26bbdd1e7da431dc23581e5033a
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2023-04-08 22:59:05 +0300

    Update Financial.jsx

--- dashboard/src/pages/Charts/Financial.jsx
+++ dashboard/src/pages/Charts/Financial.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
-import { ChartComponent, SeriesCollectionDirective, SeriesDirective } from '@syncfusion/ej2-react-charts'
+import { ChartComponent, SeriesCollectionDirective, SeriesDirective } 
+from '@syncfusion/ej2-react-charts'
+import { Header } from '../../components'
+
+import { financialChartData, FinancialPrimaryXAxis, FinancialPrimaryYAxis } from '../../data/dummy'
 
 const Financial = () => {
   return (
     <div className='w-full'>
-
+      <Header  />
     </div>
   )
 }
